d459e58d79cb6aef382039449dfb44c18212c2ac
GeHa6yKuH <132999265+GeHa6yKuH@users.noreply.github.com>
2023-06-26 21:37:34 +0300

    Add files via upload
    Initial commit

---
+++ Hometask_1/src/main/java/TaskHW/Main.java
@@ -0,0 +1,37 @@
+package TaskHW;
+
+public class Main {
+    public static void main(String[] args) {
+        char myChar = 'G';
+        System.out.println("har: " + myChar);
+        int myInt = 89;
+        System.out.println("int: " + myInt);
+        byte myByte = 4;
+        System.out.println("byte: "+ myByte);
+        short myShort = 56;
+        System.out.println("short: " + myShort);
+        float myFloat = 4.7333436F;
+        System.out.println("float: " + myFloat);
+        double myDouble = 4.355453532;
+        System.out.println("double: " + myDouble);
+        long myLong = 12121;
+        System.out.println("long: " + myLong);
+
+
+        Character myCharacter = myChar;
+        System.out.println("Character: " + myCharacter);
+        Integer myInteger = myInt;
+        System.out.println("Integer: " + myInteger);
+        Byte myByte2 = myByte;
+        System.out.println("Byte: " + myByte2);
+        Short myShort2 = myShort;
+        System.out.println("Short: " + myShort2);
+        Float myFloat2 = myFloat;
+        System.out.println("Float: " + myFloat2);
+        Double myDouble2 = myDouble;
+        System.out.println("Double: " + myDouble2);
+        Long myLong2 = myLong;
+        System.out.println("Long: " + myLong2);
+
+    }
+}
