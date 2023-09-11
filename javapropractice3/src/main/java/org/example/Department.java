e74ef94ba38c73c013903a84e9e55d30eaa2c4a0
GeHa6yKuH <132999265+GeHa6yKuH@users.noreply.github.com>
2023-09-11 15:25:37 +0300

    Add files via upload
    Init

---
+++ javapropractice3/src/main/java/org/example/Department.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Department{
+
+    private String name;
+    private Employee[] employees;
+
+    public Department(String name, Employee[] employees) {
+        this.name = name;
+        this.employees = employees;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Employee[] getEmployees() {
+        return employees;
+    }
+}
